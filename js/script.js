09dbc7f721287008fb9bc1f5ba7ef5d68778b0ec
Anton Ivanov <antishgamer@mail.ru>
2021-01-30 20:22:44 -0400

    Вторая домашка

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
-let money;
-let income; 
-let addExpenses; 
-let deposit;
-let mission; 
-let period;
-alert("Hello");
-console.log("First Homework");
+let money = 300000;
+income = "фриланс";
+addExpenses = "Интернет, Такси, Коммуналка";
+deposit = 3 > 2;
+mission = 987654;
+period = 7;
+
+console.log(typeof money, income, deposit);
+console.log('длинна строки ' + addExpenses.length + ' символов');
+console.log('Период равен ' + period + ' месяцев и' + ' Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase().split(' '));
+
+budgetDay = 300000/30;
+console.log(budgetDay);
+
+// ↓ или так правильней? ↓
+
+monthlyIncome = 900000;
+dailyBudget = monthlyIncome/30;
+console.log(dailyBudget);
